7165128e7902e903d54e124b81586a4050cdb3d3
ExtremeXT <75576145+ExtremeXT@users.noreply.github.com>
2024-01-06 22:04:22 +0200

    TWRP 11.0 -> PBRP 12.1

--- pbrp_z3s.mk
+++ pbrp_z3s.mk
@@ -1,8 +1,8 @@
 # Inherit from common AOSP config
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-# Inherit some common TWRP stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+# Inherit from common PBRP config
+$(call inherit-product, vendor/pb/config/common.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/samsung/z3s/device.mk)
@@ -16,7 +16,7 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,device/samsung/z3s/recover
 # Device identifier
 PRODUCT_RELEASE_NAME := z3s
 PRODUCT_DEVICE := z3s
-PRODUCT_NAME := twrp_z3s
+PRODUCT_NAME := pbrp_z3s
 PRODUCT_BRAND := Samsung
 PRODUCT_MODEL := Galaxy S20 Ultra
 PRODUCT_MANUFACTURER := samsung
